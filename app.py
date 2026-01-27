89657e970ef0aa0d7a7e29a834cbfe8f7ce0717b
1mNut <sebastian.nilsson@skola.taby.se>
2026-01-27 10:42:49 +0100

    .

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import *
+from werkzeug.security import *
+from functools import *
+from flask_socketio import *
+import random as rand
 import mysql.connector
 
 
+
 app = Flask(__name__)
-app.secret_key = 'key'
+app.secret_key = str(rand.randint(1, 10000))
+socketio = SocketIO(app)
 
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
     'password': '',
-    'database': 'hashed'
+    'database': 'database'
 }
+
+def login_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            return redirect(url_for('login'))
+        return func(*args, **kwargs)
+    return decorated_function
 
 def get_db_connection():
     return mysql.connector.connect(**DB_CONFIG)
@@ -22,8 +36,15 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username'].strip()
+        username = request.form['username']
         password = request.form['password']
+
+    return render_template('login.html')
+
+@app.route('/settings')
+@login_required
+def settings():
+    return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
