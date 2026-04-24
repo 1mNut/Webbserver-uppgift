acd3702b7d279c15bedc2275f7a33f2a10be1bb3
MelkerGiEk <melker.gibeckeklind@skola.taby.se>
2026-04-24 12:39:56 +0200

    pusha nya sql struktur
    Co-authored-by: 1mNut <1mNut@users.noreply.github.com>

--- ourforms.sql
+++ ourforms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 13 mars 2026 kl 14:22
+-- Tid vid skapande: 24 apr 2026 kl 12:39
 -- Serverversion: 10.4.32-MariaDB
 -- PHP-version: 8.2.12
 
@@ -35,6 +35,13 @@ CREATE TABLE `comments` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumpning av Data i tabell `comments`
+--
+
+INSERT INTO `comments` (`id`, `topic_id`, `user_id`, `content`, `date`) VALUES
+(5, 2, 1, 'salhaosf', '2026-04-16 00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +53,13 @@ CREATE TABLE `likes` (
   `user_id` int(11) NOT NULL,
   `comment_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumpning av Data i tabell `likes`
+--
+
+INSERT INTO `likes` (`id`, `user_id`, `comment_id`) VALUES
+(1, 1, 5);
 
 -- --------------------------------------------------------
 
@@ -60,6 +74,13 @@ CREATE TABLE `topics` (
   `description` varchar(200) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumpning av Data i tabell `topics`
+--
+
+INSERT INTO `topics` (`id`, `user_id`, `header`, `description`, `date`) VALUES
+(2, 1, 'Bananer är bästa frukten!', 'you heard me', '2026-04-16');
 
 -- --------------------------------------------------------
 
@@ -79,7 +100,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'Melker', 'scrypt:32768:8:1$QtwvyQeLMknqTG7V$bf4c2b30cbb14da38dac27c9b3c99ada17cf2e47bf65bf891ae0e15bee573b0bf247a498df9b7f7620d7a4c6f7c6b0fa28f0059fe7fa6bb0a0759723540150cb', 'melker.skola@gmail.com');
+(1, 'Melker', 'scrypt:32768:8:1$QtwvyQeLMknqTG7V$bf4c2b30cbb14da38dac27c9b3c99ada17cf2e47bf65bf891ae0e15bee573b0bf247a498df9b7f7620d7a4c6f7c6b0fa28f0059fe7fa6bb0a0759723540150cb', 'melker.skola@gmail.com'),
+(2, 'Sebbe', 'scrypt:32768:8:1$bhpbrskXfTL79PKP$fa3f6e7ad96f8c28ea75d8cac2601de390ea876a5fe57b3f0542a68c44b1c2fba66da68951b83288be17f546ec3b493f1595a7819ea577d3696d74eda93e3366', 'sebastian@skola.taby');
 
 --
 -- Index för dumpade tabeller
@@ -95,7 +117,8 @@ ALTER TABLE `comments`
 -- Index för tabell `likes`
 --
 ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
 -- Index för tabell `topics`
@@ -118,25 +141,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT för tabell `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT för tabell `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT för tabell `topics`
 --
 ALTER TABLE `topics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT för tabell `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
